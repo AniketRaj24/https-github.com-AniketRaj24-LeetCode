f633ff0ae06bfc15ad71bafe35204976bf22e3cd
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-08-01 23:57:16 +0530

    Time: 1 ms (76.72%), Space: 42.6 MB (22.08%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while (true) {
+            slow = fun(slow);
+            fast = fun(fun(fast));
+
+            if (slow == fast) {
+                if (slow == 1) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+
+    private int fun(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            n = n / 10;
+            sum = sum + digit * digit;
+        }
+        return sum;
+    }
+}
