e9f790a82a537777f9485590f14ab18d18f744ea
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-08-05 00:12:40 +0530

    Time: 6 ms (92.32%), Space: 55.9 MB (51.63%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.java
@@ -1,10 +1,11 @@
 class Solution {
     public int maximumSum(int[] arr) {
+        int n = arr.length;
         int noDelete = arr[0];
         int oneDelete = arr[0];
         int ans = arr[0];
 
-        for(int i = 1; i < arr.length; i++){
+        for(int i = 1; i < n; i++){
             int v1 = arr[i] + noDelete;
             int v2 = arr[i] + oneDelete;
             oneDelete = Math.max(v2, noDelete);
