6c7761801e5ef9ecd6d12885a43d6e423c85979e
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-08-08 22:19:13 +0530

    Time: 18 ms (40.85%), Space: 45.7 MB (14.63%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,50 +1,20 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int result = 0;
-        long diff = Long.MAX_VALUE;
-
+         int currsum = 0;
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
-                continue;
-            }
-            int left = i + 1;
-            int right = nums.length - 1;
-
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                long newDiff = Math.abs(target - sum);
-                if(newDiff < diff){
-                    diff = newDiff;
-                    result = sum;
-                } 
-                if(sum < target){
-                    left ++;
-                }else if(sum > target){
-                    right --;
-                }else{
-                    return sum;
-                }
-            }
-        }
-        return result;
-    }
-}
-
-//Another Approach....
- /* int currsum = 0;
-        Arrays.sort(nums);
-        int clossum = nums[0]+nums[1]+nums[2];
+        int closesum = nums[0]+nums[1]+nums[2];
 
         for(int i = 0; i< nums.length-2;i++){
             int j = i+1;
             int k = nums.length-1;
 
             while(j<k){
-               int  currsum = nums[i]+nums[j]+nums[k];
-            //    clossum = currsum-target;
-                if(Math.abs(currsum - target)<Math.abs(clossum-target)){
-                    clossum = currsum;
+               currsum = nums[i]+nums[j]+nums[k];
+               int sum1 = currsum - target;
+               int sum2 = closesum - target;
+            //    closesum = currsum-target;
+                if(Math.abs(sum1) < Math.abs(sum2)){
+                    closesum = currsum;
                 } 
 
                 if(currsum>target){
@@ -56,4 +26,7 @@ class Solution {
                 }
             }
         }
-        return clossum; */
+        return closesum;
+    }
+}
+
