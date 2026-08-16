bdcce70fa1225712ae95fc7cc10bbe007a36701e
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-08-17 00:53:29 +0530

    Time: 16 ms (19.15%), Space: 46.9 MB (39.73%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -5,19 +5,19 @@ class Solution {
 
         for(int i = 0; i < ransomNote.length(); i++){
             char a = ransomNote.charAt(i);
-            have.put(a, have.getOrDefault(a, 0) + 1);
+            need.put(a, need.getOrDefault(a, 0) + 1);
         }
          for(int i = 0; i < magazine.length(); i++){
             char b = magazine.charAt(i);
-            need.put(b, need.getOrDefault(b, 0) + 1);
+            have.put(b, have.getOrDefault(b, 0) + 1);
         }
         return fun(have, need);
     }
     private boolean fun( HashMap<Character, Integer> have,  HashMap<Character, Integer> need){
-        for(Map.Entry<Character, Integer> i : have.entrySet()){
+        for(Map.Entry<Character, Integer> i : need.entrySet()){
             char c = i.getKey();
             int fneed = i.getValue();
-            int fhave = need.getOrDefault(c,0);
+            int fhave = have.getOrDefault(c,0);
             if(fhave < fneed){
                 return false;
             }
