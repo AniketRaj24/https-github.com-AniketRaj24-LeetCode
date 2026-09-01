76a9824e525e928f837a233954dbd8da4979c8ba
Aniket Raj <113756494+AniketRaj24@users.noreply.github.com>
2026-09-01 22:41:21 +0530

    Time: 16 ms (58.07%), Space: 50.8 MB (77.14%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,37 +1,51 @@
 class Solution {
-    private int fun(int[][] matrix, int n, int m, int guess){
-        int row = n-1;
-        int col = 0;
-        int count = 0;
-        while(row >= 0 && col < m){
-            if(matrix[row][col] <= guess){
-                count = count + row + 1;
-                col++;
-            }
-            else{
-                row--;
-            }
+    static class Node{
+        int value;
+        int row;
+        int col;
+        Node(int value, int row, int col){
+            this.value = value;
+            this.row = row;
+            this.col = col;
         }
-        return count;
     }
+    static class Compare implements Comparator<Node>{
+        public int compare(Node a, Node b){
+            return a.value - b.value;
+        }
+    }
+
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
         int m = matrix[0].length;
 
-        int low = matrix[0][0];
-        int high = matrix[n-1][m-1];
-        int res = -1;
+        PriorityQueue<Node> pq = new PriorityQueue<>(new Compare());
 
-        while(low <= high){
-            int guess = (low + high)/2;
-            int ans = fun(matrix, n, m, guess);
-            if(ans < k){
-                low = guess + 1;
-            }else{
-                res = guess;
-                high = guess - 1;
-            }
+        for(int i = 0; i < n; i++){
+            pq.add(new Node(matrix[i][0], i, 0));
         }
-        return res;
+
+        int count = 0;
+        int result = -1;
+
+        while(!pq.isEmpty()){
+            Node node = pq.poll();
+            count++;
+
+            if(count == k){
+                result = node.value;
+                break;
+            }
+
+            int col = node.col;
+
+            if(col == m - 1){
+                continue;
+            }
+
+            pq.offer(new Node(matrix[node.row][col + 1], node.row, col + 1));
+        }
+
+        return result;
     }
 }
